dab573cb14b662a25f251450b31e7e87d23c3f57
Raymond Degbe <rdegbe14@gmail.com>
2021-08-01 14:47:47 -0400

    Build and style end page

--- game.js
+++ game.js
@@ -51,6 +51,7 @@ startGame = () => {
 
 getNewQuestion = () => {
   if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
     //go to the end page
     return window.location.assign("/end.html");
   }
