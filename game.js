e6232a65174fdd63ee44c2612d62ce36b6a1237d
Raymond Degbe <rdegbe14@gmail.com>
2021-08-02 01:52:46 -0400

    Create spin loader

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -37,6 +39,7 @@ fetch(
 
             return formattedQuestion;
         });
+
         startGame();
     })
     .catch((err) => {
@@ -52,6 +55,8 @@ startGame = () => {
     score = 0;
     availableQuesions = [...questions];
     getNewQuestion();
+    game.classList.remove('hidden');
+    loader.classList.add('hidden');
 };
 
 getNewQuestion = () => {
